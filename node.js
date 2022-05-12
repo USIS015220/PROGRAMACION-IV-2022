43c9a4268b7776c53c95c4145fa7315e66811760
USIS015220 <69129370+USIS015220@users.noreply.github.com>
2022-05-12 14:23:05 -0600

    Update node.js

--- node.js
+++ node.js
@@ -8,6 +8,12 @@ server.get('/chat', function (req, resp){
     resp.send('hola mundo');
 });
 
+server.get('/usuarios', function (req, resp){
+
+
+    resp.send('Bienvenido al usuario');
+});
+
 server.listen(port, function (event){
     console.log('Server running on port $(port)');
 });
